04503d7db94c36940d51b33a1188b67ed31c30c8
Erin Sheridan <SheridanErin@gmail.com>
2016-03-02 10:17:03 -0700

    updated

--- server.js
+++ server.js
@@ -1,1 +1,47 @@
+var express = require('express');
+var bodyParser = require('body-parser');
 
+var app = express(); 
+
+app.use(bodyParser.json());
+
+var messages = [];
+
+app.get('/', function( req, res ) {
+  res.set({
+    'Content-Type': 'application/json',
+    'Access-Control-Allow-Origin': '*',
+    'Access-Control-Allow-Methods': 'OPTIONS, GET, POST',
+    'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept',
+    'X-XSS-Protection': '1; mode=block',
+    'X-Frame-Options': 'SAMEORIGIN',
+    'Content-Security-Policy': "default-src 'self' devmountain.github.io"
+  }).send(JSON.stringify(messages));
+});
+
+app.post('/', function(req, res) {
+	messages.push({
+		message: req.body.message,
+		time: new Date()
+	});
+
+	res.status(200).set({
+    'Content-Type': 'application/json',
+    'Access-Control-Allow-Origin': '*',
+    'Access-Control-Allow-Methods': 'OPTIONS, GET, POST',
+    'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept',
+    'X-XSS-Protection': '1; mode=block',
+    'X-Frame-Options': 'SAMEORIGIN',
+    'Content-Security-Policy': "default-src 'self' devmountain.github.io"
+  }).send(JSON.stringify(messages));
+});
+
+app.options('/', function(req, res) {
+	res.set({
+		'Access-Control-Allow-Origin': '*',
+		'Access-Control-Allow-Methods': 'OPTIONS, GET, POST',
+		'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept'
+	}).send();
+})
+
+app.listen(8000);
